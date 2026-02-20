14bb0024d211b46fe9eba922508ba4d3d5957335
AMG2001 <mohamadamgad09@gmail.com>
2026-02-20 22:10:08 +0200

    projects initialized.

---
+++ ServiceRegisteryServer/ServiceRegisteryServer/src/main/java/tech/daif/ServiceRegisteryServer/ServiceRegisteryServerApplication.java
@@ -0,0 +1,13 @@
+package tech.daif.ServiceRegisteryServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegisteryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegisteryServerApplication.class, args);
+	}
+
+}
---
+++ ServiceRegisteryServer/ServiceRegisteryServer/src/test/java/tech/daif/ServiceRegisteryServer/ServiceRegisteryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.daif.ServiceRegisteryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisteryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
