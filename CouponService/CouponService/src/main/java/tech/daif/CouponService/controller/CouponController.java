276e882e6bd35254fa9dbb09074a162e5dc252ff
AMG2001 <mohamadamgad09@gmail.com>
2026-02-21 19:49:51 +0200

    Gateway project created, configured and implemented.

--- CouponService/CouponService/src/main/java/tech/daif/CouponService/controller/CouponController.java
+++ CouponService/CouponService/src/main/java/tech/daif/CouponService/controller/CouponController.java
@@ -3,17 +3,19 @@ package tech.daif.CouponService.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tech.daif.CouponService.model.Coupon;
 import tech.daif.CouponService.service.CouponService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/coupon")
 public class CouponController {
 
     private final CouponService couponService;
 
-    @GetMapping("/coupon/{couponName}")
+    @GetMapping("/{couponName}")
     public Coupon getCoupon(@PathVariable String couponName) {
         return couponService.getCoupon(couponName);
     }
