5ebae938cb74195919d6ab1575fb7198065aa092
AMG2001 <mohamadamgad09@gmail.com>
2026-02-23 23:49:08 +0200

    Implement config server.

--- ProductsService/ProductsService/src/main/java/tech/daif/ProductsService/controller/ProductController.java
+++ ProductsService/ProductsService/src/main/java/tech/daif/ProductsService/controller/ProductController.java
@@ -1,5 +1,6 @@
 package tech.daif.ProductsService.controller;
 
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +19,16 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
+    @Retry(name = "getProductRetry", fallbackMethod = "fallbackMethod")
     public ResponseEntity<Product> getProduct(@RequestBody ProductRequest productRequest) {
+        return ResponseEntity.ok(productService.getProduct(productRequest.productName(), productRequest.couponName()));
+    }
+
+    public ResponseEntity<Product> fallbackMethod(
+            @RequestBody ProductRequest productRequest,  // Same as original
+            Exception ex
+    ) {
+        // Your fallback logic
         return ResponseEntity.ok(productService.getProduct(productRequest.productName(), productRequest.couponName()));
     }
 }
